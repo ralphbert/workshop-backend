b49917249c143583871a86b7e6e7f1932d433217
Ralph Harrer <harrer.ralph@gmail.com>
2022-01-25 10:58:01 +0100

    set the expiration time of the token to 7 days

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { AuthController } from './auth.controller';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '7 days' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy, JwtAuthGuard],
